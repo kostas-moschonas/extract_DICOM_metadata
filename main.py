4a9eed8c3fc8b925f59a4507db9ca86aebb7e38c
kostas-moschonas <kmoschonas@outlook.com>
2025-04-03 09:58:17 +0100

    minor changes

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ metadata_extractor.dicom_files = stress_dicom_files
 stress_metadata_df = metadata_extractor.extract_metadata()
 
 # Rest sequences
-# Find DICOM files in the zip folder by series description for rest sequences
+# Find DICOM files
 rest_dicom_files = metadata_extractor.find_dicom_in_zip_folder_by_series_description(zip_folder_path, rest_sequences)
 metadata_extractor.dicom_files = rest_dicom_files
-# Extract metadata from the rest DICOM files
+# Extract metadata
 rest_metadata_df = metadata_extractor.extract_metadata()
 
-# Merge the two DataFrames
+# Merge the two dataFrames
 merged_metadata_df = pd.concat([stress_metadata_df, rest_metadata_df], ignore_index=True)
 
-# Save the merged DataFrame to a CSV file
+# Save the merged dataFrame to a CSV file
 merged_metadata_df.to_csv("output/merged_metadata_cmr.csv", index=False)
 # print("CSV saved: output/merged_metadata_cmr.csv")
